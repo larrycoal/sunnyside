583e56dfd118b4569d496dd417803b711d88280b
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-07-08 14:31:50 +0100

    beautiful web site

---
+++ index.js
@@ -0,0 +1,3 @@
+document.querySelector(".hamburger").addEventListener("click",()=>{
+    document.querySelector(".mobile-links").style.display=document.querySelector(".mobile-links").style.display==="none"? "flex":"none"
+})
